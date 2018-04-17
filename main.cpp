6f242dc57d264e9781ba6e5e25909d4ac66fe6c5
Jamal Rasool <me@trujamal.com>
2018-04-17 13:20:07 -0500

    Updated radix sort algorithm

--- main.cpp
+++ main.cpp
@@ -3,44 +3,56 @@
 #include <vector>
 #include <sstream>
 
-void print_info(std::vector<int> p_current, std::vector<int> p_temp) {
-    for(int i = p_current.size() - 1; i > 0; i--)
-        std::cout << p_current[i];
-    std::cout << p_current[0];
-    for(int i = 0; i < p_temp.size() - 1; i++)
-        std::cout<< p_temp[i];
-    std::cout << p_temp[p_temp.size() - 1] << std::endl;
+// Getting max original value to be compared to in the program
+int getMax(std::vector<int> temp_vec)
+{
+    int radix_basis = temp_vec[0];
+    for (int i = 1; i < temp_vec.size(); i++)
+        if (temp_vec[i] > radix_basis)
+            radix_basis = temp_vec[i];
+    return radix_basis;
 }
 
-std::vector<int> sorting_algorithim(std::vector a) {
-    std::vector<int> temp_vector;
+// Handles the moving of variables to the correct location to be sorted in the right position
+void sorting_helper(std::vector<int> data, int max_val)
+{
+    int output[data.size()]; // output array
+    int i, count[10] = {0};
 
-    // Radix Sort Algorithim
+    // Store count of occurrences in count[]
+    for (i = 0; i < data.size(); i++)
+        count[ (data[i]/max_val)%10 ]++;
 
-/**
- * radixSort(inout theArray:ItemArray, in n:integer, in d:integer)
-// sort n d-digit integers in the array theArray
-	for (j=d down to 1) {
-	     Initialize 10 groups to empty
-	     Initialize a counter for each group to 0
-	     for (i=0 through n-1) {
-	          k = jth digit of theArray[i]
-	          Place theArrar[i] at the end of group k
-	          Increase kth counter by 1
-	     }
-	     Replace the items  in theArray with all the items in group 0,
-  	     followed by all the items in group 1, and so on.
-	}
+    // Change count[i] so that count[i] now contains actual
+    //  position of this digit in output[]
+    for (i = 1; i < 10; i++)
+        count[i] += count[i - 1];
 
- UVA 120
- https://saicheems.wordpress.com/2013/11/05/uva-120-stacks-of-flapjacks/
+    // Build the output array
+    for (i = data.size() - 1; i >= 0; i--)
+    {
+        output[count[ (data[i]/max_val)%10 ] - 1] = data[i];
+        count[ (data[i]/max_val)%10 ]--;
+    }
 
- */
+    // Copy the output array to arr[], so that arr[] now
+    // contains sorted numbers according to current digit
+    for (i = 0; i < data.size(); i++)
+        data[i] = output[i];
+}
 
+// Radix Sort
+std::vector<int> sort_algorithim(std::vector<int> data) {
+    int max_value = getMax(data);
 
+    for (int j = 1; max_value/j > 0; j *= 10)
+        sorting_helper(data, j);
+return data;
+}
 
-
-    return temp_vector;
+void print_info(std::vector<int> info) {
+    for(auto x: info)
+        std::cout << x << ' ';
 }
 
 int main() {
@@ -48,18 +60,17 @@ int main() {
     std::vector<int> pancake_sequence;
     std::stringstream ss;
     int num_values;
-
-
+    
     while(std::cin >> line_input)
     {
         ss.str(line_input);
         while(ss >> num_values) // Placing int values into the vector
             pancake_sequence.push_back(num_values);
 
-        std::vector<int> temp  = sorting_algorithim(pancake_sequence);
+        std::vector<int> temp  = sort_algorithim(pancake_sequence);
 
-        // Print array information
-        print_info(pancake_sequence,temp);
+        // Print vector information
+        print_info(temp);
         // Clearing
         ss.clear();
         pancake_sequence.clear();
