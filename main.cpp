3c31622f5b965d7bd0dc7265c6efd9114f9eda6b
Trujamal <me@trujamal.com>
2018-04-17 22:44:14 -0500

    Final Commit

--- main.cpp
+++ main.cpp
@@ -1,64 +1,61 @@
 #include <iostream>
+#include <cstdio>
 #include <sstream>
-#include <deque>
-#include <algorithm>
-#include <vector>
 
-std::vector<int> reverse_vec(std::vector<int> a){
-    std::reverse(a.begin(), a.end());
-    return a;
-}
+/**
+ * @author Jamal Rasool
+ * @version 2.0
+ * @date 4/17/18
+ * @desc Program is designed to calculate the number of flips required for a given sequence of pancakes
+ * @note Data structure used in this assignment arrays
+ * @return exit code 0
+ */
+
 int main() {
-    std::string line_input;
-    int num_values;
 
-    while(std::cin >> line_input)
-    {
-        std::vector<int> pancake_sequence;
-        std::stringstream ss;
-        ss.str(line_input);
-        while(ss >> num_values) // Placing int values into the vector
-            pancake_sequence.push_back(num_values);
+    int const DIAMETER = 100; // setting the maximum size
+    std::string user_input;
 
-        for(std::vector<int>::iterator it=pancake_sequence.begin(); it!=pancake_sequence.end(); ++it)
-        {
-            std::vector<int>::iterator Max = max_element(it, pancake_sequence.end());
-            if(Max!=it)
-            {
-                if(Max != pancake_sequence.end()-1)
-                {
-                    reverse(Max, pancake_sequence.end());
-                    std::cout<<distance(pancake_sequence.begin(), Max)+1<<' ';
-                }
-                reverse(it,pancake_sequence.end());
-                std::cout<<distance(pancake_sequence.begin(),it)+1<<' ';
-            }
+    while(getline(std::cin, user_input)) {
+
+        // ----Declaring variables ----
+        std::stringstream sin(user_input);
+        int pancake_sequence[DIAMETER], counter_var = 0;
+        int flip[DIAMETER], iterate_arr = 0, temporary_val;
+
+        // Checking the user input for the number of characters within it; essentially for size keeping
+        while(sin >> pancake_sequence[counter_var])
+            counter_var++;
+
+        for(int i = 0; i < counter_var; i++) {
+            std::cout << " " << pancake_sequence[i]; // Displays user input
         }
-        // Clearing
-        ss.clear();
-        pancake_sequence.clear();
+
+        printf("%s\n",""); // formatting
+        for(int i = 0; i < counter_var; i++) {
+
+            int max_flip_index = counter_var-i-1;
+
+            for(int j = 0; j < counter_var-i; j++) {
+                if(pancake_sequence[max_flip_index] < pancake_sequence[j])
+                    max_flip_index = j;
+            }
+
+            if(max_flip_index == counter_var-i-1)
+                continue; // Equal values continue and don't modify contents
+            if(max_flip_index) {
+
+                flip[iterate_arr++] = counter_var-max_flip_index; // Setting index of the flipped array with the number of times it had to be moved
+                for(int j = 0, k = max_flip_index; j < k; j++, k--)
+                    temporary_val = pancake_sequence[j], pancake_sequence[j] = pancake_sequence[k], pancake_sequence[k] = temporary_val;
+            }
+            flip[iterate_arr++] = i+1; // Setting value to the array index
+            for(int j = 0, k = counter_var-i-1; j < k; j++, k--)
+                temporary_val = pancake_sequence[j], pancake_sequence[j] = pancake_sequence[k], pancake_sequence[k] = temporary_val;
+        }
+        for(int i = 0; i < iterate_arr; i++)
+            std::cout << flip[i] << " "; // Displays the flipped output.
+        printf("%s\n","0"); // formatting last zero at the end of each use-case
     }
-//    return 0;
-}
-while(getline(cin,str))
-{
-cout<<str<<endl;
-istringstream iss(str);
-std::vector<int> que;
-for(int tmp; iss>>tmp; que.push_back(tmp));
-for(std::vector<int>::iterator it=que.begin(); it!=que.end(); ++it)
-{
-std::vector<int>::iterator Max = max_element(it, que.end());
-if(Max!=it)
-{
-if(Max != que.end()-1)
-{
-reverse(Max, que.end());
-cout<<distance(que.begin(), Max)+1<<' ';
-}
-reverse(it,que.end());
-cout<<distance(que.begin(),it)+1<<' ';
-}
-}
-cout<<"0\n";
+    return 0;
 }
